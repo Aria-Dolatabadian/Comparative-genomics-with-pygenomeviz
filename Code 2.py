51a217e211925f8458502422921e5a83ef295594
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-17 14:47:57 +0800

    Create Code 2.py

---
+++ Code 2.py
@@ -0,0 +1,12 @@
+from pygenomeviz import GenomeViz
+
+name, genome_size = "Genome 01", 5000
+cds_list = ((100, 900, -1), (1100, 1300, 1), (1350, 1500, 1), (1520, 1700, 1), (1900, 2200, -1), (2500, 2700, 1), (2700, 2800, -1), (2850, 3000, -1), (3100, 3500, 1), (3600, 3800, -1), (3900, 4200, -1), (4300, 4700, -1), (4800, 4850, 1))
+
+gv = GenomeViz()
+track = gv.add_feature_track(name, genome_size)
+for idx, cds in enumerate(cds_list, 1):
+    start, end, strand = cds
+    track.add_feature(start, end, strand, label=f"CDS{idx:02d}")
+
+gv.savefig("Figure.png")
